3004bf2a3f3f2829191fea7e3afaf6c3b481da4d
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-11-04 07:18:51 +0200

    Сделали разметку activity_main, добавили NavigationView, перенесли Toolbar в AppBarLayout и погрузили это в CoordinatorLayout (нунжно будет в будущем), добавили нужную библиотеку дизайн.

--- app/src/main/java/com/omelchenkoaleks/remindme/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/remindme/MainActivity.java
@@ -2,15 +2,18 @@ package com.omelchenkoaleks.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Указываем какую тему и стили мы используем.
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
