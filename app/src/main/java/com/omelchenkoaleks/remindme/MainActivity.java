9adbb7bffdd5366d4a0547ba9d3bbf77d4804eb9
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-11-04 06:24:47 +0200

    Добавили Toolbar и кнопку search.

--- app/src/main/java/com/omelchenkoaleks/remindme/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/remindme/MainActivity.java
@@ -2,12 +2,35 @@ package com.omelchenkoaleks.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        // получаем Toolbar
+        toolbar = findViewById(R.id.toolbar);
+        // задаем заголовок
+        toolbar.setTitle(R.string.app_name);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        // ToolBar будет использовать меню
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
