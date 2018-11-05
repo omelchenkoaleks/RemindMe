aed1c00c70ad30143cedefde6ba84020e83e6074
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-11-05 06:35:51 +0200

    В NavigationView добавили содержимое - header and message_text.

--- app/src/main/java/com/omelchenkoaleks/remindme/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/remindme/MainActivity.java
@@ -2,22 +2,28 @@ package com.omelchenkoaleks.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    // Выносим layout в константу, что было сразу видно вначале, какой макет мы используем.
+    private static final int LAYOUT = R.layout.activity_main;
 
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Указываем какую тему и стили мы используем.
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
+        // Выносим логику в отдельные методы (логичиские части), чтобы не засорять метод onCreate().
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
@@ -35,5 +41,10 @@ public class MainActivity extends AppCompatActivity {
 
         // ToolBar будет использовать меню
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    // Инициализация NavigationView().
+    private void initNavigationView() {
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
 }
